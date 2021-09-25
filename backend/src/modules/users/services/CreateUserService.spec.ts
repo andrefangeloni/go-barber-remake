34ed68eca0dbd7bd39f6d1e1f28401ed3be41176
andrefangeloni <andrefangeloni@gmail.com>
2021-09-25 13:40:27 -0300

    test: create user service

---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123123',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a duplicate e-mail', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123123',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
