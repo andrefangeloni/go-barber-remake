2902bdbb0fec772b93629ece65b49b98356ed0ab
andrefangeloni <andrefangeloni@gmail.com>
2021-09-26 21:52:27 -0300

    feat: reset and forgot password routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appointmentsRouter);
 
