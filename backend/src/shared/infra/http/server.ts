3908e518a6afd78ea598350c60fb2649de0cde75
andrefangeloni <andrefangeloni@gmail.com>
2021-10-03 18:50:47 -0300

    feat: rate limiter

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 
@@ -15,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
