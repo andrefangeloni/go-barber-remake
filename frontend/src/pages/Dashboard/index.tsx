e320373ef8c400afbe5b57e479bab7436bf19d9f
andrefangeloni <andrefangeloni@gmail.com>
2021-10-07 23:45:49 -0300

    feat: styled card appointment

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { user, signOut } = useAuth();
@@ -31,6 +40,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+              <strong>André Angeloni</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -56,3 +56,97 @@ export const Profile = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  display: flex;
+  max-width: 1120px;
+  margin: 64px auto;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      margin: 0 8px;
+      border-left: 1px solid #ff9000;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    font-size: 20px;
+    font-weight: 400;
+    color: #999591;
+  }
+
+  div {
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    margin-top: 24px;
+    padding: 16px 24px;
+    border-radius: 10px;
+    background: #3e3b47;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 1px;
+      height: 80%;
+      left: 0;
+      top: 10%;
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+      margin-left: auto;
+      color: #999591;
+
+      svg {
+        margin-right: 8px;
+        color: #ff9000;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
