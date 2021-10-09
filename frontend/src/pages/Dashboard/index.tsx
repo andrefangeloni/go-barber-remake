c939033236ff5871f81033b2fe93303a4f165f3f
andrefangeloni <andrefangeloni@gmail.com>
2021-10-09 20:30:46 -0300

    feat: styled calendar

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import DayPicker, { DayModifiers } from 'react-day-picker';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
@@ -14,11 +14,26 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
+import 'react-day-picker/lib/style.css';
+
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
   const { user, signOut } = useAuth();
+
+  const handleDateChange = React.useCallback(
+    (day: Date, modifiers: DayModifiers) => {
+      if (modifiers.available) {
+        setSelectedDate(day);
+      }
+    },
+    [],
+  );
 
   return (
     <Container>
@@ -62,8 +77,77 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+                <strong>André Angeloni</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+                <strong>André Angeloni</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+                <strong>André Angeloni</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
