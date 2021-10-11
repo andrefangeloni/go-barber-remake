9be47fb666088da308b50d55abca705f9f046303
andrefangeloni <andrefangeloni@gmail.com>
2021-10-10 22:35:31 -0300

    feat: profile, create appointment and appointment created initial screens

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../screens/Dashboard';
+import CreateAppointment from '../screens/CreateAppointment';
+import AppointmentCreated from '../screens/AppointmentCreated';
+
+import Profile from '../screens/Profile';
 
 export type AppStackList = {
   Dashboard: undefined;
+  CreateAppointment: undefined;
+  AppointmentCreated: undefined;
+
+  Profile: undefined;
 };
 
 const AppStack = createNativeStackNavigator<AppStackList>();
@@ -17,6 +25,10 @@ const AuthRoutes: React.FC = () => (
     }}
   >
     <AppStack.Screen name="Dashboard" component={Dashboard} />
+    <AppStack.Screen name="CreateAppointment" component={CreateAppointment} />
+    <AppStack.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <AppStack.Screen name="Profile" component={Profile} />
   </AppStack.Navigator>
 );
 
