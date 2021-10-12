d4398065b2f615173ada669c783cf91c627e33b5
andrefangeloni <andrefangeloni@gmail.com>
2021-10-12 14:21:40 -0300

    feat: dashboard header

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -4,9 +4,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredredentials {
@@ -15,7 +22,7 @@ interface SignInCredredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredredentials): Promise<void>;
   signOut(): void;
--- mobile/src/screens/Dashboard/index.tsx
+++ mobile/src/screens/Dashboard/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Button } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { useAuth } from '../../hooks/auth';
+import { AppStackList } from '../../routes/app.routes';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
+type DashboardScreenProp = NativeStackNavigationProp<AppStackList, 'Dashboard'>;
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation<DashboardScreenProp>();
+
+  const navigateToProfile = React.useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
-      <Button title="Sair" onPress={() => signOut()} />
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={() => navigateToProfile()}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
     </Container>
   );
 };
